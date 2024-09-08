692717c47bdfe759192d0a072c15b7b1cfcf46e6
ibldzn <haytsam.adzka@gmail.com>
2024-09-08 07:08:51 +0700

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,23 @@ import JaringanSection from "@/components/dashboard/JaringanSection";
 import LatestNewsSection from "@/components/dashboard/LatestNewsSection";
 import LayananSection from "@/components/dashboard/LayananSection";
 import WhyAmalyfeSection from "@/components/dashboard/WhyAmalyfeSection";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-12">
+    <main className="flex flex-col gap-12 relative">
       <HeaderSection />
       <JaringanSection />
       <LayananSection />
       <WhyAmalyfeSection />
       <LatestNewsSection />
+      <Image
+        src="/tanya-minfy.png"
+        alt="Tanya Minfy"
+        width={120}
+        height={120}
+        className="rounded-full bg-[#e1f5ef] sticky right-20 bottom-10 ml-auto"
+      />
     </main>
   );
 }
--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,11 +1,11 @@
-import { User } from "lucide-react";
+import { Bell, User } from "lucide-react";
 import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav className="p-8 w-full flex justify-between shadow-lg">
       <Link href="/" className="font-bold text-lg">
-        Ztion
+        Amalyfe
       </Link>
       <div className="flex gap-4 text-sm items-center">
         <Link href="/beranda" className="hover:underline">
@@ -17,7 +17,11 @@ const Navbar = () => {
         <Link href="/about" className="hover:underline">
           Tentang Kami
         </Link>
-        <User size={22} className="rounded-full border border-gray-500" />
+        <Bell size={22} className="rounded-full" />
+        <div className="rounded-full shadow-xl p-2 flex gap-2 justify-center items-center">
+          <User size={22} />
+          <span className="text-xs">Profil</span>
+        </div>
       </div>
     </nav>
   );
